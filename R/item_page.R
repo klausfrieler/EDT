27d5f3037f5a747daeb87e2026e95442e3e3a383
klaus <kgf@omniversum.de>
2021-04-14 22:29:46 +0200

    Some para = param fixed

--- R/item_page.R
+++ R/item_page.R
@@ -93,8 +93,8 @@ audio_NAFC_page_flex <- function(label,
                                  prompt,
                                  choices,
                                  audio_url,
-                                 correct_answer = correct_answer,
-                                 adaptive = adaptive,
+                                 correct_answer,
+                                 adaptive = TRUE,
                                  save_answer = TRUE,
                                  get_answer = NULL,
                                  on_complete = NULL,
@@ -112,20 +112,20 @@ audio_NAFC_page_flex <- function(label,
                  id = "response_ui")
     )
   if (adaptive){
-  if(is.null(get_answer)){
-  get_answer <- function(input, ...) {
-    answer <- as.numeric(gsub("answer", "", input$last_btn_pressed))
-  }
-  validate <- function(answer, ...) !is.null(answer)
-  psychTestR::page(ui = ui, label = label,  get_answer = get_answer, save_answer = save_answer,
+    if(is.null(get_answer)){
+      get_answer <- function(input, ...) {
+        answer <- as.numeric(gsub("answer", "", input$last_btn_pressed))
+      }
+      validate <- function(answer, ...) !is.null(answer)
+      psychTestR::page(ui = ui, label = label,  get_answer = get_answer, save_answer = save_answer,
                      validate = validate, on_complete = on_complete, final = FALSE,
                      admin_ui = admin_ui)
     }
-  else
-    get_answer <- function(input, ...) {
-      answer <- as.numeric(gsub("answer", "", input$last_btn_pressed))
-      correct <- EDT::EDT_item_bank[EDT::EDT_item_bank$item_number == label,]$correct == answer
-      tibble(answer = answer,
+    else
+      get_answer <- function(input, ...) {
+        answer <- as.numeric(gsub("answer", "", input$last_btn_pressed))
+        correct <- EDT::EDT_item_bank[EDT::EDT_item_bank$item_number == label,]$correct == answer
+        tibble(answer = answer,
              label = label,
              correct = correct)
     }
@@ -138,7 +138,7 @@ EDT_item <- function(label = "",
                      correct_answer,
                      prompt = "",
                      audio_dir = "",
-                     adaptive = adaptive,
+                     adaptive = TRUE,
                      save_answer = TRUE,
                      on_complete = NULL,
                      get_answer = NULL,
@@ -146,7 +146,6 @@ EDT_item <- function(label = "",
                      ){
 
   page_prompt <- shiny::div(prompt)
-
   choices <- c("1", "2")
   audio_url <- file.path(audio_dir, audio_file)
   audio_NAFC_page_flex(label = label,
