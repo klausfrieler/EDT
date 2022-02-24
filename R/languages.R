7e90d4841771f3f0fb006d523bfc3013e2d6fc0f
klaus <kgf@omniversum.de>
2022-02-24 18:44:49 +0100

    Added formal German (de_f)

--- R/languages.R
+++ R/languages.R
@@ -3,5 +3,5 @@
 #' Lists the languages available for EDT implementations.
 #' @export
 EDT_languages <- function() {
-  c("en", "de", "ru", "nl", "it" )
+  c("en", "de", "de_f", "ru", "nl", "it", "es")
 }
